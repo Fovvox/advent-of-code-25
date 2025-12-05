acc7d35c3ac72941c8a49666eba49b4a4964284a
Yurii Kondratenko <yurii.kondratenko@consol.de>
2025-12-05 09:59:08 +0100

    day 5 part 2

--- src/aoc/fovvox/day5/Main.java
+++ src/aoc/fovvox/day5/Main.java
@@ -6,7 +6,9 @@ import aoc.fovvox.Util;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     static void main() throws IOException {
@@ -15,7 +17,7 @@ public class Main {
             .map(s -> s.split("-"))
             .map(strings ->
                 new Range(Long.parseLong(strings[0]), Long.parseLong(strings[1])))
-            .toList();
+            .collect(Collectors.toList());
         List<Long> ids = Arrays.stream(parts.getLast().split("\n"))
             .mapToLong(Long::parseLong)
             .boxed()
@@ -28,14 +30,41 @@ public class Main {
         System.out.println("Part1: " + result);
         System.out.println(timer.getFormatted());
         System.out.println();
-//
-//        rolls = Util.parseStringMatrix("inputs/day4.txt");
-//        timer.start();
-//        result = part2(rolls);
-//        timer.stop();
-//        System.out.println("Part2: " + result);
-//        System.out.println(timer.getFormatted());
-//        System.out.println();
+
+        timer.start();
+        result = part2(ranges);
+        timer.stop();
+        System.out.println("Part2: " + result);
+        System.out.println(timer.getFormatted());
+        System.out.println();
+    }
+
+    //too high 675484123462146
+    private static long part2(List<Range> ranges) {
+        ranges.sort(Comparator.comparingLong(r -> r.start));
+        List<Range> mergedRanges = new ArrayList<>();
+        long curStart = ranges.getFirst().start;
+        long curEnd = ranges.getFirst().end;
+
+        for (int i = 1; i < ranges.size(); i++) {
+            if (ranges.get(i).start <= curEnd) {
+                curEnd = Math.max(ranges.get(i).end, curEnd);
+            } else {
+                mergedRanges.add(new Range(curStart, curEnd));
+                curStart = ranges.get(i).start;
+                curEnd = ranges.get(i).end;
+            }
+        }
+        mergedRanges.add(new Range(curStart, curEnd));
+//        for (Range r : ranges) {
+//            System.out.println(r + " : " + r.size());
+//        }
+//        System.out.println("------------------");
+//        for (Range r : mergedRanges) {
+//            System.out.println(r + " : " + r.size());
+//        }
+
+        return mergedRanges.stream().mapToLong(Range::size).sum();
     }
 
     private static long part1(List<Range> ranges, List<Long> ids) {
@@ -65,6 +94,15 @@ public class Main {
 
         boolean contains(long value) {
             return value >= start && value <= end;
+        }
+
+        long size() {
+            return end - start + 1;
+        }
+
+        @Override
+        public String toString() {
+            return start + "-" + end;
         }
     }
 }
